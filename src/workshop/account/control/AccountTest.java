1f5f1c2e3b58610edbc28c86359c4d5bac56341d
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-06 17:19:58 +0900

    사용자정의 Exception throws하기

--- src/workshop/account/control/AccountTest.java
+++ src/workshop/account/control/AccountTest.java
@@ -1,6 +1,7 @@
 package workshop.account.control;
 
 import workshop.account.entity.Account;
+import workshop.account.exception.InsufficientBalanceException;
 
 public class AccountTest {
 	
@@ -29,12 +30,17 @@ public class AccountTest {
 		account2.deposit(10000);
 		System.out.println("잔액 = " + account2.getBalance());
 		
-		System.out.println("10000원 출금");
-		account2.withdraw(10000);
-		System.out.println("잔액 = " + account2.getBalance());
-		
-		System.out.println("5000원 출금");
-		account2.withdraw(5000);
-		System.out.println("잔액 = " + account2.getBalance());
+
+		try {
+			System.out.println("10000원 출금");
+			account2.withdraw(10000);
+			System.out.println("잔액 = " + account2.getBalance());
+			
+			System.out.println("5000원 출금");
+			account2.withdraw(5000);
+			System.out.println("잔액 = " + account2.getBalance());
+		} catch (InsufficientBalanceException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
