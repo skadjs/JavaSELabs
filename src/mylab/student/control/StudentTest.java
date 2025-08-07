892151410ce49b80c2c379e9d0629de9c40df939
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-08 01:54:19 +0900

    commit

--- src/mylab/student/control/StudentTest.java
+++ src/mylab/student/control/StudentTest.java
@@ -4,17 +4,26 @@ import mylab.student.entity.Student;
 import mylab.student.exception.InvalidGradeException;
 
 public class StudentTest {
-	public static void main(String[] args) {
-		//1. Student 객체 생성 - 오버로딩된 생성자(학번, 이름, 전공 필드 초기화)
-		//학번(studentId) 임의
-		Student student1 = new Student(1122334455, "김민수", "컴퓨터공학");
-		
-		//학년 필드에 값 대입 - setter
-		try {
-			student1.setGrade(3);
-			student1.setGrade(5); //5학년으로 변경
-		} catch (InvalidGradeException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    public static void main(String[] args) {
+        //1. Student 객체 생성 - 오버로딩된 생성자(학번, 이름, 전공 필드 초기화)
+        //학번(studentId) 임의
+        Student student1 = new Student(1122334455, "김민수", "컴퓨터공학");
+
+        //setter 메서드 호출로 학년 필드에 값 대입 - 3학년
+        try {
+            student1.setGrade(3);
+        } catch (InvalidGradeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //1-2. getter 메서드 호출로 값 확인
+        System.out.println(student1.getName() + " / " + student1.getMajor() + " / " + student1.getGrade() + "학년");
+
+        //setter 메서드 호출로 학년 필드 값 변경 - 5학년
+        try {
+            student1.setGrade(5); //5학년으로 변경
+        } catch (InvalidGradeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/mylab/student/entity/Student.java
+++ src/mylab/student/entity/Student.java
@@ -3,57 +3,58 @@ package mylab.student.entity;
 import mylab.student.exception.InvalidGradeException;
 
 public class Student {
-	
-	private int studentId;
-	private String name;
-	private String major;
-	private int grade;
-	
-	//Constructor Overloading(생성자 중복정의)
-	public Student(int studentId, String name, String major) {
-		setStudentId(studentId);
-		setName(name);
-		setMajor(major);
-	}
-	
-	//studentId 필드에 대한 getter, setter
-	public int getStudentId() {
-		return studentId;
-	}
-	
-	public void setStudentId(int studentId) {
-		this.studentId = studentId;
-	}
-	
-	//name 필드에 대한 getter, setter
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	//major 필드에 대한 getter, setter
-	public String getMajor() {
-		return major;
-	}
-	
-	public void setMajor(String major) {
-		this.major = major;
-	}
-	
-	//grade 필드에 대한 getter, setter
-	public int getGrade() {
-		return grade;
-	}
-	
-	public void setGrade(int grade) throws InvalidGradeException {
-		//학년이 4학년을 초과할 경우 -> 예외처리
-		if(grade > 4) {
-			String errMessage = "학년은 1~4 사이여야 합니다.";
-			throw new InvalidGradeException(errMessage);
-		} 
-		this.grade = grade;
-	}
+
+    private int studentId;
+    private String name;
+    private String major;
+    private int grade;
+
+    //Constructor Overloading(생성자 중복정의)
+    public Student(int studentId, String name, String major) {
+        setStudentId(studentId);
+        setName(name);
+        setMajor(major);
+    }
+
+    //studentId 필드에 대한 getter, setter
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    //name 필드에 대한 getter, setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //major 필드에 대한 getter, setter
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    //grade 필드에 대한 getter, setter
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) throws InvalidGradeException {
+        //학년이 4학년을 초과할 경우 -> 예외처리
+        if(grade > 4) {
+            System.out.print(grade + "학년으로 변경\n");
+            String errMessage = "학년은 1~4 사이여야 합니다.";
+            throw new InvalidGradeException(errMessage);
+        }
+        this.grade = grade;
+    }
 }
