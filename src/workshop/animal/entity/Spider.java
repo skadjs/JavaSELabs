a60917a1706ce94c40eb4f3efa8cd7f60e9466c4
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-08 10:02:26 +0900

    commit

---
+++ src/workshop/animal/entity/Spider.java
@@ -0,0 +1,13 @@
+package workshop.animal.entity;
+
+public class Spider extends Animal {
+
+	public Spider() {
+		super(8);
+	}
+	
+	@Override
+	public void eat() {
+		System.out.println("거미는 벌레를 먹어요!");
+	}
+}
