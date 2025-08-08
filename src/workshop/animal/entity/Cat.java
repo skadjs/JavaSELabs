e75f7a60edbf3fc679f8dc0238e5d757cbcfa578
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-08 09:48:22 +0900

    Cat add

---
+++ src/workshop/animal/entity/Cat.java
@@ -0,0 +1,39 @@
+package workshop.animal.entity;
+
+public class Cat extends Animal implements Pet {
+	
+	private String name;
+	
+	public Cat(String name) {
+		super(4);
+		this.name = name;
+	}
+
+	public Cat() {
+		//super(4);
+		this("");
+	}
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+}
