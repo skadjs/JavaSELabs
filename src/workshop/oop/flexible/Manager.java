7bf30302199633ec3d8933fbf91c9a875d07c05e
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-13 16:22:20 +0900

    comit

--- src/workshop/oop/flexible/Manager.java
+++ src/workshop/oop/flexible/Manager.java
@@ -1,27 +1,15 @@
 package workshop.oop.flexible;
 
-//관리자 클래스
+// 관리자 클래스
 public class Manager extends Employee {
-	private String dept;
-	
-	public Manager(String name, double salary) {
-		//this.name = name;
-		//this.salary = salary;
-		super(name, salary);
-	}
-
-	public Manager(String name, double salary, String dept) {
-		this(name, salary);
-		this.dept = dept;
-	}
-	
-	public String getDept() {
-		return dept;
-	}
-	
-	@Override
-	public void manageSalary(double rate) {
-		salary = salary + salary * (rate/100);
-		salary += 20; //20만 원을 추가로 받는다.
-	}
+    
+    public Manager (String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    
+    public void manageSalary(double rate) {
+        salary = salary+ salary*(rate/100);
+        salary += 20; // 20만 원을 추가로 받는다
+    }
 }
--- src/workshop/oop/inflexible/Manager.java
+++ src/workshop/oop/inflexible/Manager.java
@@ -1,25 +1,25 @@
 package workshop.oop.inflexible;
 
-//관리자 클래스
+// 관리자 클래스
 public class Manager {
-	String name;
-	double salary;
-	
-	public Manager(String name, double salary) {
-		this.name = name;
-		this.salary = salary;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public double getSalary() {
-		return this.salary;
-	}
-	
-	public void manageSalary(double rate) {
-		salary = salary + salary * (rate/100);
-		salary += 20; //20만 원을 추가로 받는다.
-	}
+    String name;
+    double salary;
+    
+    public Manager (String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public double getSalary() {
+        return this.salary;
+    }
+    
+    public void manageSalary(double rate) {
+        salary = salary+ salary*(rate/100);
+        salary += 20; // 20만 원을 추가로 받는다
+    }
 }
