2ae2e656c13b7afceb7c9ebcc7a1105388eda14d
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-14 09:58:28 +0900

    commit

--- src/workshop/oop/flexible/MereClerk.java
+++ src/workshop/oop/flexible/MereClerk.java
@@ -8,7 +8,9 @@ public class MereClerk extends Employee {
 		// this.salary = salary;
 		super(name, salary);
 	}
-	 
+	
+	// 오버라이딩 규칙: 부모가 상속해 준 메서드의 Signature(메서드 선언부)가 반드시 같아야 한다.
+	@Override
 	public void manageSalary(double rate) {
 		salary = salary+ salary*(rate/100);
 	}
