87f344c7b9ff788cf1563c2a1df24610b808ce77
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-14 10:30:26 +0900

    commit

--- src/workshop/book/entity/Publication.java
+++ src/workshop/book/entity/Publication.java
@@ -6,4 +6,48 @@ public class Publication {
 	private String publicationDate;
 	private int page;
 	private int price;
+	
+	public Publication() {
+		
+	}
+	
+	public Publication(String title, String publicationDate, int page, int price) {
+		super();
+		this.title = title;
+		this.publicationDate = publicationDate;
+		this.page = page;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(String publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
