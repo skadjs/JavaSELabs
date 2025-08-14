a957f452d9a3e831cc7e0e97007b6ab7265b66f9
github_skadjs <143885031+skadjs@users.noreply.github.com>
2025-08-14 16:42:18 +0900

    commit

--- src/workshop/animal/entity/Cat.java
+++ src/workshop/animal/entity/Cat.java
@@ -1,5 +1,38 @@
 package workshop.animal.entity;
 
-public class Cat {
+public class Cat extends Animal implements Pet {
+	
+	private String name;
+	
+	public Cat(String name) {
+		super(4);
+		this.name = name;
+	}
+	
+	public Cat() {
+		// super(4);
+		this(""); // 위에 있는 생성자를 호출, 인자는 공백("")
+	}
 
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+
+	@Override
+	public void play() {
+		System.out.println("고양이는 장화를 신고 놀아요!");
+		
+	}
+
+	@Override
+	public void eat() {
+		System.out.println("고양이는 생선을 먹어요!");
+	}
 }
